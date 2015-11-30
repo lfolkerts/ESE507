88f40b1b890a2ea950c4b462607509f795d0449d
Lars Folkerts <lfolkerts@lab05.ece.stonybrook.edu>
2015-11-30 12:25:50 -0500

    Script Errors-need to rerun

--- Project3/src/Run.bash
+++ Project3/src/Run.bash
@@ -6,21 +6,24 @@
 #also will not use ./gen code - we can sed directly
 #Runtime - long ~2hrs
 
-OUTFILE="../report/synthesis.csv"
+OUTFILE="../report/synthesis_lars.csv"
 
 if [ ! -d tmp ] 
 then
 	mkdir tmp
 fi
 
-cp /home/home4/pmilder/ese507/ese507setup-csh ./tmp
+cd tmp
+cp /home/home4/pmilder/ese507/ese507setup-csh .
 source tmp/ese507setup-csh
-cp /home/home4/pmilder/ese507/synthesis/scripts/setupdc.tcl ./tmp
+cp /home/home4/pmilder/ese507/synthesis/scripts/setupdc.tcl .
+cd ..
+
 echo "Bits,Matrix Size,Pipelining,Parallelism,Frequency,Slack,Slack,Comb Area,Buf Area,Non-Comb Area,Total Area,Dyn Power (uW),Leakage Power (uW)" > $OUTFILE
 
-for b in 4 8 16 32 64
+for b in 8 12 16 20
 do	
-	for k in 4 8 16 32 64
+	for k in 8 12 16 20
 	do
 		for g in 0 1
 		do
@@ -52,9 +55,9 @@ do
 				rm tmp/test_tmp #done with this file
 				cp tmp/runsynth.tcl tmp/runsynth_template.tcl #have a baseline - we only need to change frequencies in this iteration
 				if [ "$g" -eq "0" ]; then #we cheat here since we already know the clock times - saves time
-					SYNTHT="1010"
+					SYNTHT="1600"
 				else
-					SYNTHT="730"
+					SYNTHT="900"
 				fi
 				COUNT="0"
 				INC="100"
@@ -70,8 +73,9 @@ do
 					#run synthesis
 					echo "Starting $b $k $p $g $SYNTHT_SCALED $COUNT"
 					dc_shell -f runsynth.tcl >outdc.txt
-
+					
 					#outputs
+					cp outdc.txt "out/synthout_$(printf "$k")_$(printf "$p")_$(printf "$b")_$(printf "$g")_$(printf "$SYNTHT").txt"
 					#parameters
 					printf "$b,$k,$g,$p,$SYNTHT_SCALED," >>../$OUTFILE
 					
@@ -95,22 +99,24 @@ do
 					#power - need to convert units
 					for POWER_STR in "Total Dynamic Power" "Cell Leakage Power"
 					do	
+						grep "$POWER_STR" outdc.txt	
 						if  grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | grep --quiet  m  
 						then
-							POWER=$(grep "Total Dynamic Power" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
+							POWER=$(grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
 							POWER=$(bc -l <<< "$POWER*1000")
 						elif  grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | grep --quiet u
 						then
-							POWER=$(grep "Total Dynamic Power" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
+							POWER=$(grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
 						elif grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | grep --quiet  n
 						then
-							POWER=$(grep "Total Dynamic Power" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
+							POWER=$(grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+.*W" | awk '{a+=$0; printf a}')
 							POWER=$(bc -l <<< "$POWER/1000")
 						else #assumng Watts 
 							POWER=$(grep "$POWER_STR" outdc.txt | grep -P -o "[-]*[0-9]+[.]*[0-9]+")
 							POWER=$(bc -l <<< "$POWER*1000000")
 						fi
-						printf "$POWER," >>../outdc.txt
+						echo $POWER
+						printf "$POWER," >>../$OUTFILE
 					done
 					
 					#move to next frequncy to test
@@ -118,11 +124,15 @@ do
 						if grep --quiet VIOLATED outdc.txt
 						then
 							SYNTHT=$(echo "$SYNTHT+$INC" | bc -l)
-						else
+						elif grep --quiet MET outdc.txt
+                                                then
+
 							INC=$(($INC / -10))
 							SYNTHT_UPPER=$SYNTHT
 							SYNTHT=$(echo "$SYNTHT+$INC" | bc -l)
 							COUNT=1
+						else
+							COUNT=10
 						fi
 					elif [ "$COUNT" -eq "1" ]; then #decrease fine grained until max frequency is found
 						if grep --quiet VIOLATED outdc.txt 
@@ -130,8 +140,11 @@ do
 							INC=$(($INC * -10))
 							SYNTHT=$(echo "$SYNTHT_UPPER+$INC" | bc -l)
 							COUNT=2
-						else
+						elif grep --quiet MET outdc.txt
+						then
 							SYNTHT=$(echo "$SYNTHT+$INC" | bc -l)
+						else
+							COUNT=10
 						fi
 					elif [ "$COUNT" -ge "2" ]; then #get a range of data points
 						SYNTHT=$(echo "$SYNTHT+$INC" | bc -l)
@@ -148,5 +161,5 @@ do
 		done
 	done
 done				
-
+echo "Sythesis Done"
 exit 0
